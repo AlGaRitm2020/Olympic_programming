e8d14b3e4672246324c896c1a4d105fcb6c66e8b
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-05-22 12:40:02 +0400

    added queen_сuts solution

---
+++ cmake-build-debug/CMakeFiles/Olympic_programming.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Olympic_programming.dir/Matrix.cpp.o"
+  "CMakeFiles/Olympic_programming.dir/queen_сuts.cpp.o"
+  "Olympic_programming"
+  "Olympic_programming.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Olympic_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
